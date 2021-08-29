6e5b258cfa8504f327ccd81f65b7e173f991f93d
justoj.jaramillo <justoj.jaramillo@gmail.com>
2021-08-29 15:13:12 -0500

    Models and Responses create. The classes added are TotalTimeWorked, WorkingHOursEntry and Response

---
+++ hw_azure_functions.Common/Models/TotalTimeWorked.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw_azure_functions.Common.Models
+{
+    public class TotalTimeWorked
+    {
+        public int EmployeeId { get; set; }
+        public DateTime Date { get; set; }
+        public int MinutesWorked { get; set; }
+    }
+}
---
+++ hw_azure_functions.Common/Models/WorkingHoursEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw_azure_functions.Common.Models
+{
+    public class WorkingHoursEntry
+    {
+        public int EmployeeId { get; set; }
+        public DateTime RecordDate { get; set; }
+        public bool RecordType { get; set; }
+        public bool Consolidated { get; set; }
+    }
+}
